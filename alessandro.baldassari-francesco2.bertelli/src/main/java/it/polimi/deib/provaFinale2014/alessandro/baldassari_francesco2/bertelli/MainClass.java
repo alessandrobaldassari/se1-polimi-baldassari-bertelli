fc19a48b6aaea3058d470a50957197d1915a86fb
franceassisi <francesco2.bertelli@mail.polimi.it>
2014-05-13 14:17:17 +0200

    The slave's first commit.

---
+++ alessandro.baldassari-francesco2.bertelli/src/main/java/it/polimi/deib/provaFinale2014/alessandro/baldassari_francesco2/bertelli/MainClass.java
@@ -0,0 +1,10 @@
+package it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
