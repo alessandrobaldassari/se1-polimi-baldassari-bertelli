27067b0980dc88ce85c8a81c2786223fa28cc65c
Alessandro Baldassari <alessandro.baldassari@live.it>
2014-06-01 17:34:39 +0200

    added FenceTest

--- alessandro.baldassari-francesco2.bertelli/src/test/java/it/polimi/deib/provaFinale2014/alessandro/baldassari_francesco2/bertelli/businessmodel/positionableTest/FenceTest.java
+++ alessandro.baldassari-francesco2.bertelli/src/test/java/it/polimi/deib/provaFinale2014/alessandro/baldassari_francesco2/bertelli/businessmodel/positionableTest/FenceTest.java
@@ -1,12 +1,17 @@
 package it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.positionableTest;
 
+import static org.junit.Assert.*;
 import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.Match;
 import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.map.GameMap;
 import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.map.GameMapFactory;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.positionable.Fence;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.positionable.Fence.FenceAlreadyPlacedException;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.positionable.Fence.FenceType;
 import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.utilities.Identifiable;
 import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.utilities.SingletonElementAlreadyGeneratedException;
 
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class FenceTest {
 
@@ -15,6 +20,8 @@ public class FenceTest {
 	 */
 	static GameMap map;
 	static DummyMatchIdentifier dummyMatchIdentifier;
+	static Fence fenceNonFinal = new Fence(FenceType.NON_FINAL);
+	static Fence fenceFinal = new Fence(FenceType.FINAL);
 	
 	@BeforeClass
 	public static void setUpBeforeClass()
@@ -27,6 +34,51 @@ public class FenceTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		try {
+			fenceNonFinal.place(map.getRoadByUID(1));
+		} catch (FenceAlreadyPlacedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			fenceFinal.place(map.getRoadByUID(2));
+		} catch (FenceAlreadyPlacedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void Fence(){
+		assertTrue(fenceNonFinal.equals(fenceNonFinal));
+	}
+	
+	@Test
+	public void isFinal(){
+		assertTrue(fenceFinal.isFinal());
+		assertFalse(fenceNonFinal.isFinal());
+	}
+	
+	@Test
+	public void isPlaced(){
+		Fence fence = new Fence(FenceType.FINAL);
+		assertTrue(fenceFinal.isPlaced());
+		assertFalse(fence.isPlaced());
+	}
+	
+	@Test (expected = FenceAlreadyPlacedException.class)
+	public void place() throws FenceAlreadyPlacedException{
+		Fence fence = new Fence(FenceType.NON_FINAL);
+		fence.place(map.getRoadByUID(3));
+		assertTrue(fence.getPosition().getUID() == 3);
+		assertTrue(fence.isPlaced());
+		fence.place(map.getRoadByUID(1));
+	}
+	
+	@Test
+	public void equals(){
+		assertTrue(fenceFinal.equals(fenceFinal));
+		assertFalse(fenceFinal.equals(fenceNonFinal));
 	}
 	
 	static class DummyMatchIdentifier implements Identifiable<Match>{
