7e2199972fe8f6da35295a12a41d20aa352358bf
Alessandro Baldassari <alessandro.baldassari@live.it>
2014-06-01 16:10:31 +0200

    Added CharacterDoesntMoveException Test

---
+++ alessandro.baldassari-francesco2.bertelli/src/test/java/it/polimi/deib/provaFinale2014/alessandro/baldassari_francesco2/bertelli/businessmodel/positionableTest/CharacterDoesntMoveExceptionTest.java
@@ -0,0 +1,53 @@
+package it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.positionableTest;
+
+import static org.junit.Assert.*;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.Match;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.character.animal.AdultOvine;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.character.animal.AnimalFactory;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.character.animal.AdultOvine.AdultOvineType;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.map.GameMap;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.map.GameMapFactory;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.map.Region.RegionType;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.positionableTest.CharacterTest.DummyMatchIdentifier;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.utilities.Identifiable;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.utilities.SingletonElementAlreadyGeneratedException;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.positionable.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CharacterDoesntMoveExceptionTest {
+
+	static AdultOvine sheep;
+	static AnimalFactory animalFactory ;
+	static DummyMatchIdentifier dummyMatchIdentifier;
+	
+	@BeforeClass
+	public static void setUpBeforeClass()
+	{
+		dummyMatchIdentifier = new DummyMatchIdentifier();
+		try {
+			animalFactory = AnimalFactory.newAnimalFactory(dummyMatchIdentifier);
+		} catch (SingletonElementAlreadyGeneratedException e) {
+			e.printStackTrace();
+		}
+		sheep = (AdultOvine) animalFactory.newAdultOvine("Sheep", AdultOvineType.SHEEP);
+	}
+	
+	@Test
+	public void CharacterDoesntMoveException(){
+		CharacterDoesntMoveException exception = new CharacterDoesntMoveException(sheep);
+		assertTrue(exception.getCharacter() == sheep);
+	}
+	
+	public static class DummyMatchIdentifier implements Identifiable<Match>{
+
+		public DummyMatchIdentifier() {
+			// TODO Auto-generated constructor stub
+		}
+
+		public boolean isEqualsTo(Identifiable<Match> otherObject) {
+			// TODO Auto-generated method stub
+			return true;
+		}
+	}
+}
