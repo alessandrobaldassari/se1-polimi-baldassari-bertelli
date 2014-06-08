78199fdd0299fcace7db0e3efea2c7330a1e8525
alessandrobaldassari <alessandro.baldassari@mail.polimi.it>
2014-06-08 14:59:25 +0200

    GameMapElementTest

---
+++ alessandro.baldassari-francesco2.bertelli/src/test/java/it/polimi/deib/provaFinale2014/alessandro/baldassari_francesco2/bertelli/businessmodel/map/GameMapElementTest.java
@@ -0,0 +1,35 @@
+package it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.map;
+
+import static org.junit.Assert.*;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.map.GameMapElement.GameMapElementType;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.map.Region.RegionType;
+
+import org.junit.Test;
+
+public class GameMapElementTest {
+
+	Region region1 = new Region(RegionType.HILL, 1);
+	Region region2 = new Region(RegionType.CULTIVABLE, 2);
+	Road road1 = new Road(1,1,region1,region2);
+	Road road2 = new Road(2,2,region1,region2);
+	
+	@Test
+	public void gameMapElementType() {
+		assertTrue(region1.getGameMapElementType() == GameMapElementType.REGION);
+		assertTrue(road1.getGameMapElementType() == GameMapElementType.ROAD);
+	}
+	
+	@Test
+	public void getUID(){
+		assertTrue(region2.getUID() == 2);
+		assertTrue(road1.getUID() == 1);
+	}
+	
+	@Test
+	public void equals(){
+		assertTrue(region1.equals(region1));
+		assertFalse(region1.equals(region2));
+		assertTrue(road1.equals(road1));
+	}
+
+}
