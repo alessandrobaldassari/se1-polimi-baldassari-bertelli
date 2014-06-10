2738092913b2365eded6d197a7b04cd67aba011d
Alessandro Baldassari <alessandro.baldassari@live.it>
2014-06-10 15:54:30 +0200

    MatchControllerTest missing result calc

---
+++ alessandro.baldassari-francesco2.bertelli/src/test/java/it/polimi/deib/provaFinale2014/alessandro/baldassari_francesco2/bertelli/businessmodel/match/MatchControllerTest.java
@@ -0,0 +1,66 @@
+package it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.match;
+
+import static org.junit.Assert.*;
+
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.WrongMatchStateMethodCallException;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.bank.Bank;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.bank.BankFactory;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.map.GameMap;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.map.GameMapFactory;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.businessmodel.match.Match.MatchState;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.testutilities.DummyMatchIdentifier;
+import it.polimi.deib.provaFinale2014.alessandro.baldassari_francesco2.bertelli.utilities.SingletonElementAlreadyGeneratedException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MatchControllerTest{
+
+	Match match;
+	GameMap gameMap;
+	Bank bank;
+	DummyMatchIdentifier requesterDummyMatchIdentifier;
+	MatchResultsCalculator matchResultsCalculator;
+	
+	@Before
+	public void setUp(){
+		requesterDummyMatchIdentifier = new DummyMatchIdentifier(1);
+		try {
+			gameMap = GameMapFactory.getInstance().newInstance(requesterDummyMatchIdentifier);
+		} catch (SingletonElementAlreadyGeneratedException e) {
+			e.printStackTrace();
+		}
+		try {
+			bank = BankFactory.getInstance().newInstance(requesterDummyMatchIdentifier);
+		} catch (SingletonElementAlreadyGeneratedException e) {
+			e.printStackTrace();
+		}
+		match = new Match(gameMap, bank);
+	
+	}
+
+	@Test
+	public void MatchResultCalculator() {
+		match.setMatchState(MatchState.CALCULATING_RESULTS);
+		try {
+			matchResultsCalculator = new MatchResultsCalculator(match);
+		} catch (WrongMatchStateMethodCallException e) {
+			assertFalse(true);
+		}
+		match.setMatchState(MatchState.TURNATION);
+		try {
+			matchResultsCalculator = new MatchResultsCalculator(match);
+		} catch (WrongMatchStateMethodCallException e) {
+			assertTrue(e.getActualState() == MatchState.TURNATION);
+		}
+		match.setMatchState(MatchState.CALCULATING_RESULTS);
+		try {
+			matchResultsCalculator = new MatchResultsCalculator(null);
+		}
+		catch (IllegalArgumentException e) {
+			assertTrue(true);
+		} catch (WrongMatchStateMethodCallException e) {
+			assertTrue(false);
+		}
+	}
+}
